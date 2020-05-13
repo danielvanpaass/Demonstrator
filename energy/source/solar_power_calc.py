b633ba079c6ff2be8c17b826b0ade4a94dd5dccc
danielvanpaass <38658984+danielvanpaass@users.noreply.github.com>
2020-05-13 20:28:47 +0200

    Get data from txt instead of HTTP API

--- energy/source/solar_power_calc.py
+++ energy/source/solar_power_calc.py
@@ -1,51 +1,16 @@
 import json
-import requests
 import pandas as pd
 import numpy as np
+
 import matplotlib.pyplot as plt
+import pvlib
 
-# from timezonefinder import TimezoneFinder
+# choose good model params
+temp_params = pvlib.temperature.TEMPERATURE_MODEL_PARAMETERS['sapm']['open_rack_glass_glass']
 
-"""get API access to Irradiance and Temp of 2019, with input: location"""
-
-
-def gather_ir(lat, lon):
-    api_base = 'https://www.renewables.ninja/api/'
-    s = requests.session()
-    s.headers = {'Authorization': 'Token 39046b41231860f99468167ba101617bcb782f05'}
-    url = api_base + 'data/pv'
-    args = {
-        'local_time': 'true',
-        'raw': 'true',
-        'lat': lat,
-        'lon': lon,
-        'date_from': '2018-12-31',
-        'date_to': '2019-12-31',
-        'dataset': 'merra2',
-        'capacity': 1.0,
-        'system_loss': 0.1,
-        'tracking': 0,
-        'tilt': 35,
-        'azim': 180,
-        'format': 'json'
-    }
-    r = s.get(url, params=args)
-    parsed_response = json.loads(r.text)
-    data = pd.read_json(json.dumps(parsed_response['data']), orient='index')
-    # trim this to year data of Ir and Temp
-    timezone = 1  # should be calculated from localtime in data
-    start_year = 24 - timezone
-    data = data[start_year:data.size - timezone]
-    global_ir = data.to_numpy()[:, 2] + data.to_numpy()[:, 3]
-    temp = data.to_numpy()[:, 4]
-    return global_ir, temp
-
-
-# An implementation of a solar energy model in Netherlands
-# size, angle(panel), irradiance, efficiency, temperature reliance
-# import time
-# import math
-# import datetime
+#get weather paramameters
+with open('weather.txt') as json_file:
+    weather = json.load(json_file)
 
 
 """Power calculation solar panel with ambient temp as operating temp"""
@@ -57,13 +22,12 @@ def power_calc(length, width, efficiency, coefficient, irradiance, temperature):
     return p_out
 
 
-# lat and lon as input, global_ir and Temp array as output of 2019
-global_ir, temp = gather_ir(51.998, 4.373)
-
 # Datasheet imported values
 length, width, efficiency, coefficient = 1.956, 0.992, 0.186, -0.39
 
 # calculate power per solar panel
+global_ir = np.array(weather['irradiance_diffuse']) + np.array(weather['irradiance_direct'])
+temp = np.array(weather['temp'])
 power = power_calc(length, width, efficiency, coefficient, global_ir, temp)
 # timehour = np.arange(0, temp.size)
 
