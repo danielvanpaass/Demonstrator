520452f02cc37852c5818bf17e444af272c062e4
danielvanpaass <38658984+danielvanpaass@users.noreply.github.com>
2020-05-14 15:13:59 +0200

    add different tilts

--- energy/source/solar_power_calc.py
+++ energy/source/solar_power_calc.py
@@ -9,8 +9,20 @@ import pvlib
 temp_params = pvlib.temperature.TEMPERATURE_MODEL_PARAMETERS['sapm']['open_rack_glass_glass']
 
 # get weather paramameters
-with open('weather.txt') as json_file:
-    weather = json.load(json_file)
+with open('wind.txt') as json_file:
+    wind = json.load(json_file)
+wind = np.array(wind['wind'])
+with open('solar30.txt') as json_file:
+    solar30 = json.load(json_file)
+global_ir_30 = np.array(solar30['irradiance_diffuse']) + np.array(solar30['irradiance_direct'])
+with open('solar35.txt') as json_file:
+    solar35 = json.load(json_file)
+global_ir_35 = np.array(solar35['irradiance_diffuse']) + np.array(solar35['irradiance_direct'])
+with open('solar40.txt') as json_file:
+    solar40 = json.load(json_file)
+global_ir_40 = np.array(solar40['irradiance_diffuse']) + np.array(solar40['irradiance_direct'])
+temp = np.array(solar40['temp'])
+
 
 """Power calculation solar panel with ambient temp as operating temp"""
 
@@ -24,18 +36,27 @@ def power_calc(length, width, efficiency, coefficient, irradiance, temperature):
 # Datasheet imported values
 length, width, efficiency, coefficient = 1.956, 0.992, 0.186, -0.39
 
-# calculate power per solar panel
-global_ir = np.array(weather['irradiance_diffuse']) + np.array(weather['irradiance_direct'])
-temp = np.array(weather['temp'])
-wind = np.array(weather['wind'])
-tcell = pvlib.temperature.sapm_cell(global_ir, temp, wind, **temp_params)
-power = power_calc(length, width, efficiency, coefficient, global_ir, tcell)
+# calculate power per solar panel for each tilt
+tcell = pvlib.temperature.sapm_cell(global_ir_30, temp, wind, **temp_params)
+power30 = power_calc(length, width, efficiency, coefficient, global_ir_30, tcell)
+tcell = pvlib.temperature.sapm_cell(global_ir_35, temp, wind, **temp_params)
+power35 = power_calc(length, width, efficiency, coefficient, global_ir_35, tcell)
+tcell = pvlib.temperature.sapm_cell(global_ir_40, temp, wind, **temp_params)
+power40 = power_calc(length, width, efficiency, coefficient, global_ir_40, tcell)
 
 """Main function to be called in this file, for total power out in a year per hour"""
 
 
-def power_out_solar(N_panels):
+def power_out_solar(N_panels, tilt_panel):
     data = {}  # empty dictionary
+    if tilt_panel == 30:
+        power = power30
+    elif tilt_panel == 35:
+        power = power35
+    elif tilt_panel == 40:
+        power = power40
+    else:
+        raise ValueError('tilt not defined')
     tot_power = power * N_panels
     tot_power = np.around(tot_power.astype(np.float), 3)  # rounding for reducing the message size
     data.update({'power': tot_power.tolist()})
