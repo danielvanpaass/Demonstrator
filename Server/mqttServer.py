4a9cbb7ddbcb59447e2c9a7b5eb15958767a9614
danielvanpaass <38658984+danielvanpaass@users.noreply.github.com>
2020-05-14 18:05:36 +0200

    update communication

--- Server/mqttServer.py
+++ Server/mqttServer.py
@@ -3,54 +3,52 @@ import json
 import paho.mqtt.client as mqtt
 from Server import Solartabtest
 
-
-def start_server():
-    global data_out
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            client.connected_flag = True  # set flag
-            print("connected OK Returned code=", rc)
-        else:
-            print("Bad connection Returned code= ", rc)
+global data_out
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        client.connected_flag = True  # set flag
+        print("connected OK Returned code=", rc)
+    else:
+        print("Bad connection Returned code= ", rc)
 
 
-    def on_message(client, userdata, message):
-        m = message.payload.decode("utf-8")
-        print("message received ", str(m))
-        print("message topic=", message.topic)
-        Solartabtest.dash_update_solar(json.loads(m))
-        # m = message.payload.decode("utf-8")
-        # with open('data.json', 'w', encoding='utf-8') as f:
-        #     json.dump(m, f, ensure_ascii=False, indent=4)
+def on_message(client, userdata, message):
+    m = message.payload.decode("utf-8")
+    print("message received ", str(m))
+    print("message topic=", message.topic)
+    Solartabtest.dash_update_solar(json.loads(m))
+    # m = message.payload.decode("utf-8")
+    # with open('data.json', 'w', encoding='utf-8') as f:
+    #     json.dump(m, f, ensure_ascii=False, indent=4)
 
 
-    def getMAC(interface='eth0'):
-        # Return the MAC address used for client ID
-        try:
-            str = open('/sys/class/net/%s/address' % interface).read()
-        except:
-            str = 'alias_server_notpi'
-        return str[0:17]
+def getMAC(interface='eth0'):
+    # Return the MAC address used for client ID
+    try:
+        str = open('/sys/class/net/%s/address' % interface).read()
+    except:
+        str = 'alias_server_notpi'
+    return str[0:17]
 
 
-    #broker_address = "raspberrypi"  # server Pi name (you can also use IP address here)
-     broker_address="test.mosquitto.org" #use external broker
+broker_address = "raspberrypi"  # server Pi name (you can also use IP address here)
+# broker_address="test.mosquitto.org" #use external broker
 
-    # instantiate client with MAC client ID for the session
-    client = mqtt.Client(getMAC('eth0'))
-    # set to false to be toggled by on_connect
-    client.connected_flag = False
-    # bind call back functions
-    client.on_connect = on_connect
-    client.on_message = on_message
+# instantiate client with MAC client ID for the session
+client = mqtt.Client(getMAC('eth0'))
+# set to false to be toggled by on_connect
+client.connected_flag = False
+# bind call back functions
+client.on_connect = on_connect
+client.on_message = on_message
 
-    client.connect(broker_address)
-    # in the loop, call back functions can be activated
-    client.loop_start()
-    # client.subscribe("to_clients")
-    client.subscribe("to_dash")
-    # initial publish of power values
-    while True:
-        Solartabtest.connect_and_run_dash(client)
-    # client.publish("demon/data",power_out_solar(600))
-    # client.publish("demon/data","OFF")#publish
+client.connect(broker_address)
+# in the loop, call back functions can be activated
+client.loop_start()
+# client.subscribe("to_clients")
+client.subscribe("to_dash")
+# initial publish of power values
+while True:
+    Solartabtest.connect_and_run_dash(client)
+# client.publish("demon/data",power_out_solar(600))
+# client.publish("demon/data","OFF")#publish
--- runServerAndClient.py
+++
@@ -1,5 +0,0 @@
-from Client import mqttClient
-from Server import mqttServer
-
-mqttClient.start_client()
-mqttServer.start_server()
