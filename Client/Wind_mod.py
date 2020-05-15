52aa8db26b0c9a4286b0fca3fdada9d50fc928c3
Benjamin <B.E.Visser@student.tudelft.nl>
2020-05-15 13:01:26 +0200

    First wind model#2

--- Client/Wind_mod.py
+++ Client/Wind_mod.py
@@ -1,7 +1,9 @@
 "An implementation of a wind energy model for a wind turbine"
 import math
 
-windturbine_params= WIND_PARAMETERS['WES5'] = {
+type = 'WES5'
+
+windturbine_params= WIND_PARAMETERS[type] = {
 
         'WES5': {'diameter': 5, 'height': 12, 'cut_inspeed': 3, 'cut_outspeed':20},
         'Turbine2': {'diameter': 5, 'height': 12, 'cut_inspeed': 3, 'cut_outspeed':20},
@@ -15,23 +17,29 @@ windturbine_params= WIND_PARAMETERS['WES5'] = {
 # rho = pressure / (gas_coefficient * temperature)
 # lambda = v_d/v_u, blade tip speed /  wind speed
 # blade tip speed = angular speed turbine * Diameter/2 / wind speed
-# Coefficient power = (1 + lambda) * (1 - (lambda)^2)/2, max = 0.59
-def power_calc_wind(wind_speed, temperature, **windturbine_params):
+# Coefficient power = (1 + lambda) * (1 - (lambda)^2)/2, max = 0.59, lambda = tsr
+def power_calc_wind(wind_speed, temperature, windturbine_params):
+    # angular velocity of turbine is related to tip speed ratio to determine efficiency
+    omega_turbine = 60 * wind_speed * tsr / (math.pi * windturbine_params.diameter)
 
-    blade_tip_speed = (omega_turbine * **windturbine_params.diameter) / (wind_speed)
+    blade_tip_speed = (omega_turbine * windturbine_params.diameter) / wind_speed
 
-    lamda = blade_tip_speed / wind_speed
+    tsr = blade_tip_speed / wind_speed
 
-    # R_g (gasconstant), pressure is pressure at windturbine height
+    # R_g (gas constant), pressure is pressure at wind turbine height
+    pressure = 760 * math.exp(−0.00012 * windturbine_params.height)
     rho = pressure / (R_g * temperature)
 
-    efficiency = ((1 + lamda) * (1 - (lamda)^2))/2
+    efficiency = ((1 + tsr) * (1 - tsr ^ 2)) / 2
 
-    area = (**windturbine_params.diameter/4)*math.pi
+    area = ((windturbine_params.diameter)^2/4)*math.pi
 
-    wind_power = 0.5 * rho * area * (wind_speed)^3 * efficiency
+    wind_power = 0.5 * rho * area * wind_speed^3 * efficiency
 
-    p_out = wind_power
+    if wind_speed < windturbine_params.cut_inspeed | wind_speed > windturbine_params.cut_outspeed:
+        p_out = 0
+    else:
+        p_out = wind_power
     return p_out
 
 
