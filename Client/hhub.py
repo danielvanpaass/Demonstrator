9f33bde7d66f956ee023f58180b184421f55bd75
jprozestraten <jp.rozestraten.99@gmail.com>
2020-05-18 11:07:22 +0200

    bugfixes and comments

--- Client/hhub.py
+++ Client/hhub.py
@@ -1,10 +1,10 @@
 import enum
 import smbus
 
-from . import __USEPINS__
+from Client import __USEPINS__
 
 # Enumeration class for the types of models
-class Model(enum.Enum):
+class Model(enum.IntEnum):
 	SOLARPV = enum.auto()
 	WINDTURBINE = enum.auto()
 	HOUSEHOLD = enum.auto()
@@ -25,6 +25,7 @@ DAC_MAX = 255
 BITS_ADC = 8
 ADC_MAX = 255
 
+# Use this function to set a value for the table-top models
 def setModel(power, model, power_min, power_max):
 	# Exceptions for user input
 	if (power < power_min):
@@ -32,28 +33,27 @@ def setModel(power, model, power_min, power_max):
 	elif (power > power_max):
 		raise Exception('power should not be smaller than power_max.')
 
-	dac_value = (power - power_min) * DAC_MAX / (power_max - power_min)
-	address = base_address + model * 2
+	dac_value = round((power - power_min) * DAC_MAX / (power_max - power_min))
+	address = base_address + (model - 1) * 2
 	
 	if __USEPINS__ == True:
 		return smbus.write_byte(address, dac_value)
 	else:
+		print("Trying to write via GPIO pins, but __USEPINS__ is set to False")
+		print("Writing value ", dac_value, " to address ", hex(address))  
 		return 0
 
+# Use this function to get a value for the table-top model sensors
 def getModel(model, power_min, power_max):
-	# Exceptions for user input
-	if (power < power_min):
-		raise Exception('power should not be smaller than power_min.')
-	elif (power > power_max):
-		raise Exception('power should not be smaller than power_max.')
-	
-	address = base_address + model * 2 + 1
+	address = base_address + (model - 1) * 2 + 1
 	
 	if __USEPINS__ == True:
 		adc_value = smbus.read_byte(address)
 	else:
 		adc_value = ADC_MAX
-	
+		print("Trying to read via GPIO pins, but __USEPINS__ is set to False")
+		print("Storing ", adc_value, " in adc_value as dummy from address ", hex(address))
+		
 	return (power_max - power_min) / ADC_MAX * adc_value + power_min
 
 # def getConnected():
