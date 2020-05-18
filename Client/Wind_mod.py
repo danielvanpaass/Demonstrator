dd06d9d52d6de324647165f57d280eae532d57b5
Benjamin <B.E.Visser@student.tudelft.nl>
2020-05-18 12:15:32 +0200

    Wind model#3

--- Client/Wind_mod.py
+++ Client/Wind_mod.py
@@ -3,9 +3,9 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-type = 'WES5'
-wind = np.array([2, 2, 3, 15, 18, 4, 19, 21, 18, 16])
-temp = np.array([2, 5, 8, 12, 15, 17, 20, 22, 18, 10])
+type_turbine = 'WES5'
+wind = [2, 2, 3, 15, 18, 4, 19, 21, 18, 16]
+temp = [2, 5, 8, 12, 15, 17, 20, 22, 18, 10]
 
 WIND_PARAMETERS = {
 
@@ -15,7 +15,7 @@ WIND_PARAMETERS = {
     'Turbine4': {'diameter': 5, 'height': 12, 'cut_inspeed': 3, 'cut_outspeed': 20},
 }
 
-windturbine_params = WIND_PARAMETERS[type]
+windturbine_params = WIND_PARAMETERS[type_turbine]
 
 
 # P = 0.5 * rho * A * v^3 * Coefficient power
@@ -25,7 +25,7 @@ windturbine_params = WIND_PARAMETERS[type]
 # Coefficient power = (1 + lambda) * (1 - (lambda)^2)/2, max = 0.59, lambda = tsr
 def power_calc_wind(wind_speed, temperature, **windturbine_params):
     # angular velocity of turbine is related to tip speed ratio to determine efficiency
-    omega_turbine = wind_speed / windturbine_params['diameter']/2
+    omega_turbine = wind_speed
 
     blade_tip_speed = (omega_turbine * windturbine_params['diameter'] / 2) / wind_speed
 
@@ -35,20 +35,24 @@ def power_calc_wind(wind_speed, temperature, **windturbine_params):
     pressure = 101325
     rho = pressure / (287.058 * (temperature + 273.15))
 
-    efficiency = ((1 + tsr) * (1 - (tsr ** 2))) / 2
+    efficiency = ((1 + tsr) * (1 - (tsr ** 2)) / 2)
 
     area = ((windturbine_params['diameter']) ** 2 / 4) * math.pi
 
     wind_power = 0.5 * rho * area * wind_speed ** 3 * efficiency
 
-    if wind_speed < windturbine_params['cut_inspeed']:
-        p_out = 0
-    else:
-        p_out = wind_power
-    return p_out, efficiency
+    return wind_power, efficiency
 
 
-windpower, c = power_calc_wind(wind, temp, **windturbine_params)
+#bln = (wind >= windturbine_params['cut_inspeed']).any() and (wind <= windturbine_params['cut_outspeed']).any()
+
+
+if windturbine_params['cut_inspeed'] <= wind <= windturbine_params['cut_outspeed']:
+     windpower, c = power_calc_wind(wind, temp, **windturbine_params)
+else:
+     windpower = 0
+
+
 print(windpower)
 plt.plot(windpower)
 plt.show()
