09c261b65441cf6ac1bda5cb8d5257c4add6ed5b
Benjamin <B.E.Visser@student.tudelft.nl>
2020-05-18 16:26:10 +0200

    Wind model cubic#2

--- Client/Wind_polynom.py
+++ Client/Wind_polynom.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 type_turbine = 'WES5'
-wind = [2, 2, 3, 15, 18, 4, 19, 21, 18, 16]
+wind = [2, 2, 3.1, 4, 5, 6, 7, 8, 9, 10]
 
 WIND_PARAMETERS = {
 
@@ -19,16 +19,22 @@ windturbine_params = WIND_PARAMETERS[type_turbine]
 def power_calc_wind(wind_speed, **windturbine_params):
     a = windturbine_params['P_rated'] / (windturbine_params['V_rated'] ** 3 - windturbine_params['cut_inspeed'] ** 3)
 
-    b = windturbine_params['V_rated'] / (windturbine_params['V_rated'] ** 3 - windturbine_params['cut_inspeed'] ** 3)
+    b = windturbine_params['V_rated'] ** 3 / (windturbine_params['V_rated'] ** 3 - windturbine_params['cut_inspeed'] ** 3)
     wind_speed = np.array(wind_speed)
-    wind_power = a * wind_speed ** 3 - b * windturbine_params['P_rated']
+    wind_power = a * (wind_speed ** 3) - (b * windturbine_params['P_rated'])
 
     for x in range(0, len(wind_speed)):
         if wind_speed[x]<windturbine_params['cut_inspeed'] or wind_speed[x]>windturbine_params['cut_outspeed']:
             wind_power[x] = 0
+
+    #for x in range(0, len(wind_speed)):
+     #   if wind_power[x]>windturbine_params['P_rated']:
+       #     wind_power[x] = windturbine_params['P_rated']
 
     return wind_power.tolist()
 
 
 windpower = power_calc_wind(wind, **windturbine_params)
 print(windpower)
+plt.plot(windpower)
+plt.show()
