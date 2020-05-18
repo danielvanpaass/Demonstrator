fef8ee55073d6ecb8ccefddd3807218f2c20e218
danielvanpaass <38658984+danielvanpaass@users.noreply.github.com>
2020-05-18 15:57:52 +0200

    to array

--- Client/Wind_polynom.py
+++ Client/Wind_polynom.py
@@ -20,14 +20,14 @@ def power_calc_wind(wind_speed, **windturbine_params):
     a = windturbine_params['P_rated'] / (windturbine_params['V_rated'] ** 3 - windturbine_params['cut_inspeed'] ** 3)
 
     b = windturbine_params['V_rated'] / (windturbine_params['V_rated'] ** 3 - windturbine_params['cut_inspeed'] ** 3)
-
+    wind_speed = np.array(wind_speed)
     wind_power = a * wind_speed ** 3 - b * windturbine_params['P_rated']
 
     for x in range(0, len(wind_speed)):
         if wind_speed[x]<windturbine_params['cut_inspeed'] or wind_speed[x]>windturbine_params['cut_outspeed']:
             wind_power[x] = 0
 
-    return wind_power
+    return wind_power.tolist()
 
 
 windpower = power_calc_wind(wind, **windturbine_params)
