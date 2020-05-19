b41fe79ebf55fc322cc157c97e9c113902ed7f2c
Victor van Doorn <victorvdoorn@hotmail.com>
2020-05-19 14:34:37 +0200

    Versie zonder tabs maar werkt

--- Server/Solartabtest.py
+++ Server/Solartabtest.py
@@ -1,16 +1,14 @@
 import json
-
 import plotly.express as px
 import pandas as pd
 import time
-
-
 
 import dash
 from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import dash_core_components as dcc
 import base64
+
 data_change = False
 data = {}
 dh = {'power_solar': [1, 2, 3]}
@@ -25,37 +23,38 @@ colors = {
 test_png = 'TUD.png'
 test_base64 = base64.b64encode(open(test_png, 'rb').read()).decode('ascii')
 
-app.layout = html.Div([
+app.layout = html.Div(children=[
     html.H1(
         children='Energy System Integrator Demonstrator',
         style={
             'textAlign': 'center',
             'color': colors['text']
         }
-    ), html.Img(src='data:image/png;base64,{}'.format(test_base64)),
-    dcc.Tabs([
-        dcc.Tab(label='Input Parameters', children=[
-            html.Button('Refresh', id='button', n_clicks=0),
-            html.Div('PV Parameters'),
-            dcc.Input(id='input', value=20, type='number'),
-            dcc.Dropdown(
-                    id='dropdown',
-                    options=[
-                        {'label': '30 degrees tilt', 'value': 30},
-                        {'label': '35 degrees tilt', 'value': 35},
-                        {'label': '40 degrees tilt', 'value': 40}
-                    ],
-                    value=30
-    ),      html.Div('Load Parameters'),
-            dcc.Input(id='input load', value=160, type='number'),
-    dcc.Tab(label='Output', children=[
-            html.Div(id='output-pv'),
-            dcc.Graph(id='pvpower'),
-            dcc.Graph(id='loadpower')
-        ]),
-
-        ]),
-    ])
+    ),
+    html.Img(src='data:image/png;base64,{}'.format(test_base64)),
+    html.H3('PV Parameters',
+            style={'color': colors['text']}),
+    html.Div('Number of PV panels'),
+    dcc.Input(id='input', value=20, type='number'),
+    html.Button('Refresh', id='button', n_clicks=0),
+    html.Div('Tilt of the PV panels'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[
+            {'label': '30 degrees', 'value': 30},
+            {'label': '35 degrees', 'value': 35},
+            {'label': '40 degrees', 'value': 40}
+        ],
+        value=30
+    ),
+    html.H3('Load Parameters',
+            style={'color': colors['text']}),
+    html.Div('Number of houses in the neighbourhood'),
+    dcc.Input(id='input load', value=160, type='number'),
+    html.Button('Refresh', id='buttonload', n_clicks=0),
+    html.Div(id='output-pv'),
+    dcc.Graph(id='pvpower'),
+    dcc.Graph(id='loadpower')
 ])
 
 
@@ -64,20 +63,20 @@ def dash_update_solar(dict):
     dh.update(dict)
     data_change = True
 
+
 @app.callback(Output('pvpower', 'figure'),
               [Input('button', 'n_clicks')],
-                state=[State('input', 'value'),
-                       State('dropdown','value')
-               ],
-              )
+              state=[State('input', 'value'),
+                     State('dropdown', 'value')
+                     ])
 def update_graph_live(n, z, k):
     global data_change
-    while(data_change == False):
+    while (data_change == False):
         pass
     data_change = False
     figure = {
         'data': [
-            {'x': dh['time'], 'y': dh['power_solar'], 'type': 'line', 'name': 'SF'}
+            {'x': dh['time'], 'y': dh['power_solar'], 'type': 'line', 'name': 'PV'}
         ],
         'layout': {
             'title': ' Power output'
@@ -85,14 +84,14 @@ def update_graph_live(n, z, k):
     }
     return figure
 
+
 @app.callback(Output('loadpower', 'figure'),
-              [Input('button', 'n_clicks')],
-                state=[State('input load', 'value')
-               ],
-              )
-def update_graph_live_load(n, z,):
+              [Input('buttonload', 'n_clicks')],
+              state=[State('input load', 'value')
+                     ])
+def update_graph_live_load(n, z, ):
     global data_change
-    while(data_change == False):
+    while (data_change == False):
         pass
     data_change = False
     figure = {
@@ -109,17 +108,16 @@ def update_graph_live_load(n, z,):
 def connect_and_run_dash(client):
     @app.callback(
         Output(component_id='output-pv', component_property='children'),
-        [Input('button', 'n_clicks')],
-        state=[State('input', 'value'), State('dropdown','value'), State('input load','value')
-               ],)
-    def update_output(n_clicks, panelvalue, tiltvalue, loadvalue):
+        [Input('button', 'n_clicks'), Input('buttonload', 'n_clicks')],
+        state=[State('input', 'value'), State('dropdown', 'value'), State('input load', 'value')
+               ], )
+    def update_output(n_clicks,n_click, panelvalue, tiltvalue, loadvalue):
         data.update({'N_solar': panelvalue})
         data.update({'tilt_panel': tiltvalue})
         data.update({'N_load': loadvalue})
         client.publish("to_clients", json.dumps(data))
 
     app.run_server(debug=False)
-
 
 
 if __name__ == '__main__':
