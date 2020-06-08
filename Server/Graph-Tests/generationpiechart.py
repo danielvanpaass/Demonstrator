e7713012dfe57737ec261cccd4c47325e12bcf71
Victor van Doorn <victorvdoorn@hotmail.com>
2020-06-08 16:36:19 +0200

    piecharts in subplot and added hydrogen storage + number of wind turbines

--- Server/Graph-Tests/generationpiechart.py
+++ Server/Graph-Tests/generationpiechart.py
@@ -1,5 +1,5 @@
 import plotly.graph_objects as go
-
+from plotly.subplots import make_subplots
 data = {'pv':[1,4,1,2,4,2],
          'wind':[1,2,5,3,2,0],
         'net':[10,2,-5,0,2,0]}
@@ -16,23 +16,25 @@ tot_other=tot_net*0.03
 labels=['PV','Wind','Natural Gas','Coal','Oil','Nuclear','Other']
 share=[tot_pv,tot_wind,tot_gas,tot_coal,tot_oil,tot_nuclear,tot_other]
 
-fig = go.Figure(data=[go.Pie(labels=labels, values=share)])
+# fig = go.Figure(data=[go.Pie(labels=labels, values=share)])
+# fig.show()
+
+fig = make_subplots(rows=1, cols=2, specs=[[{"type": "pie"}, {"type": "pie"}]])
+
+
+fig.add_trace(go.Pie(
+     values=share,
+     labels=labels,
+     domain=dict(x=[0, 0.5]),
+     name="Share of Energy source"),
+     row=1, col=1)
+
+fig.add_trace(go.Pie(
+     values=[27, 11, 4],
+     labels=["Green", "Grey", "Unknown",
+             ],
+     domain=dict(x=[0.5, 1.0]),
+     name="Green vs grey"),
+    row=1, col=2)
+
 fig.show()
-
-def sumNegativeInts(listInt):
-    m = 0
-    for x in listInt:
-        if x < 0:
-            m+=x
-    return(int(m))
-print(sumNegativeInts(data['net']))
-
-def sumPositiveInts(listInt):
-    m = 0
-    for x in listInt:
-        if x > 0:
-            m+=x
-    return(int(m))
-print(sumPositiveInts(data['net']))
-
-#data.update({'share': })
