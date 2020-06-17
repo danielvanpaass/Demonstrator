60a5da490b6a27bc6c5602635a50bb77de5bb80c
Benjamin <B.E.Visser@student.tudelft.nl>
2020-06-17 15:04:10 +0200

    no loss

--- Client/windmod.py
+++ Client/windmod.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 WIND_PARAMETERS = {
-    #'WES5': {'P_rated': 2500, 'V_rated': 8.5, 'height': 12, 'cut_inspeed': 3.0, 'cut_outspeed': 20, 'diameter': 5},
-    'V90-2MW': {'P_rated': 2000000, 'V_rated': 13, 'height': 80, 'cut_inspeed': 4.0, 'cut_outspeed': 25, 'diameter': 90},
+    # 'WES5': {'P_rated': 2500, 'V_rated': 8.5, 'height': 12, 'cut_inspeed': 3.0, 'cut_outspeed': 20, 'diameter': 5},
+    'V90-2MW': {'P_rated': 2000000, 'V_rated': 13, 'height': 80, 'cut_inspeed': 4.0, 'cut_outspeed': 25,
+                'diameter': 90},
     'Aria55+': {'P_rated': 57000, 'V_rated': 11, 'height': 19, 'cut_inspeed': 3.0, 'cut_outspeed': 25, 'diameter': 19},
     'Hummer60': {'P_rated': 60000, 'V_rated': 7.5, 'height': 12, 'cut_inspeed': 2.5, 'cut_outspeed': 20,
                  'diameter': 25},
@@ -15,7 +16,8 @@ WIND_PARAMETERS = {
     'Aria60': {'P_rated': 60000, 'V_rated': 11.0, 'height': 19, 'cut_inspeed': 3.0, 'cut_outspeed': 25, 'diameter': 19},
 }
 
-#calculated wind power in KW
+
+# calculated wind power in KW
 def power_calc_wind(wind_speed, turbine_type):
     selected_turbine = WIND_PARAMETERS[turbine_type]
 
@@ -24,26 +26,45 @@ def power_calc_wind(wind_speed, turbine_type):
     winddelta = wind_speed ** 3 - selected_turbine['cut_inspeed'] ** 3
 
     wind_power = winddelta * selected_turbine['P_rated'] / (
-                selected_turbine['V_rated'] ** 3 - selected_turbine['cut_inspeed'] ** 3)
-
-    # power losses connection to bus and step-up voltage
-    p_open = 200
-    v_grid = 400
-    resistance_c = 0.11
-    resistance_load = 2.4
-    p_load = (((wind_power - p_open) / v_grid) ** 2) * resistance_load
-    p_wt = wind_power - p_open - p_load
-    p_losses = wind_power - p_wt + (p_wt ** 2 * resistance_c / (v_grid ** 2))
+            selected_turbine['V_rated'] ** 3 - selected_turbine['cut_inspeed'] ** 3)
 
     for x in range(0, len(wind_speed)):
         if wind_speed[x] < selected_turbine['cut_inspeed'] or wind_speed[x] > selected_turbine['cut_outspeed']:
             wind_power[x] = 0
-            p_losses[x] = 0
 
     for x in range(0, len(wind_speed)):
         if wind_power[x] > selected_turbine['P_rated']:
             wind_power[x] = selected_turbine['P_rated']
 
     # power output
-    p_out_wind = wind_power/1000
+    p_out_wind = wind_power / 1000
     return p_out_wind
+
+
+turbine_type = 'Hummer60'
+windspeed = np.arange(0, 25, 0.5)
+
+powerwind = power_calc_wind(windspeed, turbine_type)
+plt.plot(windspeed, powerwind)
+plt.xlabel('Wind speed [m/s]')
+plt.ylabel('Power [kW]')
+plt.title('Power curve of Hummer60')
+plt.annotate('V_rated',
+             xy=(7.5, 60),
+             xytext=(5.5, 64), arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='right', verticalalignment='top'
+             )
+plt.annotate('V_cutin',
+             xy=(2.5, 0),
+             xytext=(2.5, 8), arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='right',verticalalignment='bottom'
+             )
+plt.annotate('V_cutoff',
+             xy=(20, 60),
+             xytext=(19.5,65), arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='right', verticalalignment='bottom'
+             )
+plt.ylim(-0.1, 70)
+plt.xlim(-0.1, 22)
+plt.savefig('Powercurvehummer60.png', bbox_inches='tight')
+plt.show()
